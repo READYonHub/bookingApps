f98895553c32b0096431bd7f11e19b8f10eb39f9
READYonHub <readyemail3@gmail.com>
2024-03-08 12:12:06 +0100

    kijavitot hiba

--- Build a Movie Seat Booking App using JavaScript - JavaScript Beginner Projects/script.js
+++ Build a Movie Seat Booking App using JavaScript - JavaScript Beginner Projects/script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const movivieSelect = document.getElementById('movie');
+const movieSelect = document.getElementById('movie');
 
 populateUI();
-let ticketPrice = +movivieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //console.log(typeof ticketPrice)
 
@@ -17,9 +17,7 @@ function setMovieData(movieIndex, moviePrice) {
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    const seatsIndex = [...selectedSeats].map(function (seat) {
-        return [...seats].indexOf(seat);
-    })
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
     //console.log(seatsIndex);//visszaadja indexelt sorszámát a helynek
 
@@ -27,15 +25,15 @@ function updateSelectedCount() {
 
     const selectedSeatsCount = selectedSeats.length;
 
-    count.innerHTML = selectedSeatsCount;
-    total.innerHTML = selectedSeatsCount * ticketPrice;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
 
     //console.log(selectedSeats)
 }
 
 //localstorage adatok
 function populateUI() {
-    const selectedSeats = JSON.stringify(localStorage.getItem('selectedSeats'))
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
     //console.log(selectedSeats)
 
@@ -46,25 +44,30 @@ function populateUI() {
             }
         });
     }
-    const selecttedMovieIndex = localStorage.getItem('selectedMovieIndex');
-    if (selecttedMovieIndex != null) {
-        movivieSelect.selectedIndex = selecttedMovieIndex;
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
     }
+
+    movieSelect.addEventListener('change', (e) => {
+        ticketPrice = +e.target.value;
+        //console.log(e.target.selectedIndex, e.target.value)//visszaadja a kivalasztott iroda undexet es az erteket
+        setMovieData(e.target.selectedIndex, e.target.value);
+        updateSelectedCount();
+    });
+
+    container.addEventListener('click', (e) => {
+        //console.log(e.target);//visszaadja a kattintott elem kódját ami a konténerben van
+        if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+            //console.log(e.target);//ha masodik feltetel nelkul logolunk, akkor minden seat nevu osztalynak kiirja a kodjat
+            e.target.classList.toggle('selected');
+
+            updateSelectedCount();
+        }
+    });
+
+    // intial count and total
+    updateSelectedCount();
 }
-
-movivieSelect.addEventListener('change', (e) => {
-    ticketPrice = +e.target.value;
-    //console.log(e.target.selectedIndex, e.target.value)//visszaadja a kivalasztott iroda undexet es az erteket
-    setMovieData(e.target.selectedIndex, e.target.value);
-    updateSelectedCount();
-})
-
-container.addEventListener('click', (e) => {
-    //console.log(e.target);//visszaadja a kattintott elem kódját ami a konténerben van
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        //console.log(e.target);//ha masodik feltetel nelkul logolunk, akkor minden seat nevu osztalynak kiirja a kodjat
-        e.target.classList.toggle('selected');
-    }
-    updateSelectedCount();
-})
-updateSelectedCount();
